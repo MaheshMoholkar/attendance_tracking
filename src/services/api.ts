cc93dca364538c68a11c2901143060981f7527c7
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-05-20 01:00:28 +0530

    add modify student endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,10 +4,14 @@ import { StudentData } from "./types";
 const BASE_URL = "http://localhost:5000";
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
+export const getStudents = async () => {
+  return (await axiosInstance.get("api/v1/students")).data;
+};
+
 export const createStudent = async (data: StudentData) => {
   return await axiosInstance.post("api/v1/student", data);
 };
 
-export const getStudents = async () => {
-  return (await axiosInstance.get("api/v1/students")).data;
+export const modifyStudent = async (data: StudentData) => {
+  return await axiosInstance.put("api/v1/student", data);
 };
--- src/services/mutations.ts
+++ src/services/mutations.ts
@@ -1,11 +1,21 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createStudent } from "./api";
+import { createStudent, modifyStudent } from "./api";
 import { StudentData } from "./types";
 
 export function useCreateStudent() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: StudentData) => createStudent(data),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["students"] });
+    },
+  });
+}
+
+export function useModifyStudent() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: StudentData) => modifyStudent(data),
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: ["students"] });
     },
